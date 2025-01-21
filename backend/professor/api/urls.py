993e4c34c6a8b7e1f7d5557dd5e197c619e20ada
miguelsacardo <miguelsacardo3@gmail.com>
2025-01-21 16:43:29 -0300

    fazendo os metodos crud para usar as urls da api. 21/01/2025

--- backend/professor/api/urls.py
+++ backend/professor/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-from .views import listar_professores
+from .views import listar_professores, ProfessoresView, ProfessoresDetailView
 
 urlpatterns = [
     path('professores', listar_professores),
+    path('prof', ProfessoresView.as_view()), #como "ProfessoresView" é uma classe, deve-se colocar "as_view()"
+    path('id/<int:pk>', ProfessoresDetailView.as_view())
 ]
--- backend/professor/api/views.py
+++ backend/professor/api/views.py
@@ -4,11 +4,34 @@ from .serializer import *
 from rest_framework import views
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import status
 
 @api_view(['GET', 'POST'])
 def listar_professores(request):
     if request.method == 'GET':
-        queryset = Cadastro.objects.all()
+        queryset = Cadastro.objects.all() #captura todos os dados da tabela cadastro. Isso não se faz no POST, por isso nele não há essa linha
         serializer = CadastroSerializer(queryset, many=True)
         return Response(serializer.data)
     
+    elif request.method == 'POST':
+        serializer = CadastroSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+
+class ProfessoresView(ListCreateAPIView): #o objetivo dessa classe é deixar como um formulário para podermos preencher
+    queryset = Cadastro.objects.all()
+    serializer_class = CadastroSerializer
+
+
+class ProfessoresDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Cadastro.objects.all()
+    serializer_class = CadastroSerializer
+    
+
