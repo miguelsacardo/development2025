383bb470b97505d52fc7657bcdd07d04bdb6e55c
miguelsacardo <miguelsacardo3@gmail.com>
2025-02-11 16:56:07 -0300

    crud completo no frontend, falta apenas fazer uma busca por ID. 11/02/2025

--- backend/front/src/components/home/index.jsx
+++ backend/front/src/components/home/index.jsx
@@ -2,23 +2,15 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { FaPencilAlt, FaRegTrashAlt } from "react-icons/fa";
 import { CgAdd } from "react-icons/cg";
+import { CiSearch } from "react-icons/ci";
 import "./style.css";
 import ModalProfessores from "../modal";
-
 
 export default function Home() {
   const [dados, setDados] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
   const token = localStorage.getItem("token");
-  // const [create, setCreate] = useState({
-  //   ni: "",
-  //   nome: "",
-  //   email: "",
-  //   cel: "",
-  //   ocup: ""
-  // })
-  const[professorSelecionado, setProfessorSelecionado] = useState(null)
-
+  const [professorSelecionado, setProfessorSelecionado] = useState(null);
 
   useEffect(() => {
     if (!token) return;
@@ -43,29 +35,28 @@ export default function Home() {
     fetchData();
   }, []);
 
-  const editar = async (id) => {console.log(id)};
-
-
-  // const handleInput = (event) => {
-  //   setCreate({ ...create, [event.target.name]: event.target.value })
-  // }
-
-  // const criar = async () => {
-  //   try {
-  //     const response = await axios.post("http://127.0.0.1:8000/api/professores",
-  //       create, {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     })
-
-  //     console.log(response)
-  //   } catch (error) {
-
-  //   }
-
-  // }
-
+  const atualizar = async (professorAtualizado) => {
+    try {
+      const response = await axios.put(
+        `http://127.0.0.1:8000/api/id/${professorAtualizado.id}`,
+        {
+          ni: professorAtualizado.ni,
+          nome: professorAtualizado.nome,
+          email: professorAtualizado.email,
+          cel: professorAtualizado.cel,
+          ocup: professorAtualizado.ocup
+        },{
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
+      setDados(dados.map((professor)=>professor.id === professorAtualizado.id ? professorAtualizado: professor))
+      setModalOpen(false)
+    } catch (error) {
+      console.error(error)
+    }
+  };
 
   const apagar = async (id) => {
     if (window.confirm("Tem certeza? ")) {
@@ -74,60 +65,39 @@ export default function Home() {
           headers: {
             Authorization: `Bearer ${token}`,
           },
-        }
-        )
-        setDados(dados.filter(professor => professor.id !== id))
+        });
+        setDados(dados.filter((professor) => professor.id !== id));
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     }
-  }
-
-  const ModalProfessores = ({ isOpen, onClose }) => {
-    if (!isOpen) return null;
-
-    return (
-      <div className="modal-overlay">
-        <div className="modal-container">
-          <button className="close-button" onClick={onClose}>×</button>
-          <h2>Cadastro de Professor</h2>
-          <input type="text" placeholder="NI"/>
-          <input type="text" placeholder="Nome"/>
-          <input type="email" placeholder="Email"/>
-          <input type="tel" placeholder="Celular"/>
-          <input type="text" placeholder="Ocupação"/>
-          <button type="submit">Salvar</button>
-        </div>
-      </div>
-    );
   };
 
-  const criar = async(novoProfessor)=>{
-    console.log("novoProfessor: ", novoProfessor)
 
-    try{
-      const response = await axios.post('http://127.0.0.1:8000/api/professores',
+  const criar = async (novoProfessor) => {
+    console.log("novoProfessor: ", novoProfessor);
+
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/professores",
         {
           ni: novoProfessor.ni,
           nome: novoProfessor.nome,
           email: novoProfessor.email,
           cel: novoProfessor.cel,
-          ocup: novoProfessor.ocup
+          ocup: novoProfessor.ocup,
         },
         {
-          headers:{
-            Authorization: `Bearer ${token}`
-          }
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-      )
-      setDados([...dados, novoProfessor])
-      setModalOpen(false)
-    }catch(error){
-
-    }
-  }
-
-
+      );
+      setDados([...dados, novoProfessor]);
+      setModalOpen(false);
+      console.log(response);
+    } catch (error) {}
+  };
 
   return (
     <>
@@ -137,12 +107,13 @@ export default function Home() {
             <div className="content">
               <div className="lista">
                 <li key={professor.id}>
-                  {professor.nome} | {professor.email} | {professor.cel} | {professor.ocup}
+                  {professor.nome} | {professor.email} | {professor.cel} |{" "}
+                  {professor.ocup}
                 </li>
               </div>
 
               <div className="btn-lista">
-                <button onClick={() => editar(professor.id)}>
+                <button onClick={() => { setModalOpen(true), setProfessorSelecionado(professor)}}>
                   <FaPencilAlt />
                 </button>
                 <button onClick={() => apagar(professor.id)}>
@@ -153,15 +124,31 @@ export default function Home() {
           ))}
         </ul>
       </div>
+
       <div className="buttons-crud-methods">
-        <button onClick={() => setModalOpen(true), setProfessorSelecionado(null)}>
+        <button
+          onClick={() => {
+            setModalOpen(true), setProfessorSelecionado(null);
+          }}
+        >
           <CgAdd className="btn-icons" />
         </button>
 
-        <ModalProfessores isOpen={modalOpen} onClose={() => setModalOpen(false)} 
-          professorSelecionado = {professorSelecionado}
-          setProfessorSelecionado = {setProfessorSelecionado}
-          criar = {criar}/>
+        <div>
+          <input />
+        </div>
+        <div>
+          <CiSearch className="procurar" />
+        </div>
+
+        <ModalProfessores
+          isOpen={modalOpen}
+          onClose={() => setModalOpen(false)}
+          professorSelecionado={professorSelecionado}
+          setProfessorSelecionado={setProfessorSelecionado}
+          criar={criar}
+          atualizar={atualizar}
+        />
       </div>
       {/* buttons */}
     </>
--- backend/front/src/components/modal/index.jsx
+++ backend/front/src/components/modal/index.jsx
@@ -1,44 +1,108 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import './style.css'
 
-const ModalProfessores = ({ isOpen, onClose }) => {
-  const handleInput = (event) => {
-    setCreate({ ...create, [event.target.name]: event.target.value })
-  }
-
-  const criar = async () => {
-    try {
-      const response = await axios.post("http://127.0.0.1:8000/api/professores",
-        create, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-
-      console.log(response)
-    } catch (error) {
-
-    }
-
-  }
-
+const ModalProfessores = ({
+  isOpen,
+  onClose,
+  professorSelecionado,
+  criar,
+  atualizar,
+}) => {
   if (!isOpen) return null;
+  console.log("Prof selecionado: ", professorSelecionado);
 
-  return (
-    <div className="modal-overlay">
-      <div className="modal-container">
-        <button className="close-button" onClick={onClose}>×</button>
-        <h2>Cadastro de Professor</h2>
-        <input type="text" placeholder="NI" value={create.ni} onChange={handleInput} name="ni" />
-        <input type="text" placeholder="Nome" value={create.nome} onChange={handleInput} name="nome" />
-        <input type="email" placeholder="Email" value={create.email} onChange={handleInput} name="email" />
-        <input type="tel" placeholder="Celular" value={create.cel} onChange={handleInput} name="cel" />
-        <input type="text" placeholder="Ocupação" value={create.ocup} onChange={handleInput} name="ocup" />
-        <button type="submit" onClick={criar}>Salvar</button>
+  const [id, setId] = useState([professorSelecionado?.id || ""]);
+  // Aqui estamos criando um estado chamado 'id', que começa com um array contendo o valor de `professorSelecionado?.id`.
+  // - Se `professorSelecionado` for definido e tiver um `id`, o estado será um array com esse `id`.
+  // - Se `professorSelecionado` não for definido ou não tiver um `id`, o estado será um array com uma string vazia (['']).
+  // Isso permite que o estado 'id' seja configurado de acordo com a seleção do professor.
+
+  const [ni, setNi] = useState(professorSelecionado?.ni || "");
+  const [nome, setNome] = useState(professorSelecionado?.nome || "");
+  const [email, setEmail] = useState(professorSelecionado?.email || "");
+  const [cel, setCel] = useState(professorSelecionado?.cel || "");
+  const [ocup, setOcup] = useState(professorSelecionado?.ocup || "");
+
+  // assim que eu abrir o modal, o useEffect entra
+  useEffect(() => {
+    if (professorSelecionado) {
+      setId(professorSelecionado.id || "");
+      setNi(professorSelecionado.ni || "");
+      setNome(professorSelecionado.nome || "");
+      setEmail(professorSelecionado.email || "");
+      setCel(professorSelecionado.cel || "");
+      setOcup(professorSelecionado.ocup || "");
+    } else {
+      setId("");
+      setNi("");
+      setNome("");
+      setCel("");
+      setEmail("");
+      setOcup("");
+    }
+  }, [professorSelecionado]); //toda vez que tem um novo professor, o useEffect roda novamente
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // não recarregar a página
+    const novoProfessor = { ni, nome, email, cel, ocup };
+    if (professorSelecionado) { //se o professor selecionado não for nulo,
+      atualizar({ ...professorSelecionado, ...novoProfessor }); // ... -> manenha os dados que existe e então altere ou adicione
+    } else {
+      criar(novoProfessor)
+    }
+  };
+
+  return(
+    <main className="modal_overlay">
+      <div className="modal_container">
+        <button className="close_button" onClick={onClose}>x</button>
+        <h2>{professorSelecionado ? "Editar": "Cadastrar"}</h2>
+        <div className="body_modal">
+          <div className="caixa1">
+            <form onSubmit={handleSubmit}>
+              <input type="text" 
+                className="ni_modal"
+                value={ni}
+                onChange={(e)=>setNi(e.target.value)}
+                placeholder="NI"
+              />
+              <input type="text" 
+                className="nome_modal"
+                value={nome}
+                onChange={(e)=>setNome(e.target.value)}
+                placeholder="NOME"
+              />
+              <input type="text" 
+                className="email_modal"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                placeholder="EMAIL"
+              />
+              <input type="text" 
+                className="cel_modal"
+                value={cel}
+                onChange={(e)=>setCel(e.target.value)}
+                placeholder="CELULAR"
+              />
+              <input type="text" 
+                className="ocup_modal"
+                value={ocup}
+                onChange={(e)=>setOcup(e.target.value)}
+                placeholder="OCUP"
+              />
+              <button type="submit">{professorSelecionado ? "Atualizar" : "Salvar"}</button>
+            </form>
+          </div>
+          <div className="caixa2">
+            <div className="foto">
+                
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    </main>
+  )
 };
 
-
-export default ModalProfessores;
+export default ModalProfessores
