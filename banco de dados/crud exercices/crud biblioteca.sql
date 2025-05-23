ef6fa39b690c9f7eb581b0b2d37adae27e365251
miguelsacardo <miguelsacardo3@gmail.com>
2025-05-23 15:53:19 -0300

    terminando os exercicios de crud

--- banco de dados/crud exercices/crud biblioteca.sql
+++ banco de dados/crud exercices/crud biblioteca.sql
@@ -2,13 +2,11 @@
 DROP DATABASE IF EXISTS biblioteca;
 CREATE DATABASE IF NOT EXISTS biblioteca;
 USE biblioteca;
-
 -- Tabela de níveis de associação
 CREATE TABLE nivel_associacao (
  id INT AUTO_INCREMENT PRIMARY KEY,
  nome VARCHAR(50) NOT NULL
 );
-
 -- Tabela de usuários
 CREATE TABLE usuario (
  id INT AUTO_INCREMENT PRIMARY KEY,
@@ -19,7 +17,6 @@ CREATE TABLE usuario (
  nivel_id INT,
  FOREIGN KEY (nivel_id) REFERENCES nivel_associacao(id)
 );
-
 -- Tabela de autores
 CREATE TABLE autor (
  id INT AUTO_INCREMENT PRIMARY KEY,
@@ -27,7 +24,6 @@ CREATE TABLE autor (
  data_nascimento DATE,
  biografia TEXT
 );
-
 -- Tabela de categorias
 CREATE TABLE categoria (
  id INT AUTO_INCREMENT PRIMARY KEY,
@@ -47,9 +43,11 @@ CREATE TABLE livro_autor (
  autor_id INT,
  PRIMARY KEY (livro_id, autor_id),
  FOREIGN KEY (livro_id) REFERENCES livro(id),
- FOREIGN KEY (autor_id) REFERENCES autor(id));
- 
- CREATE TABLE livro_categoria (
+ FOREIGN KEY (autor_id) REFERENCES autor(id)
+);
+
+-- Relacionamento N:N entre livros e categorias
+CREATE TABLE livro_categoria (
  livro_id INT,
  categoria_id INT,
  PRIMARY KEY (livro_id, categoria_id),
@@ -67,7 +65,6 @@ CREATE TABLE emprestimo (
  FOREIGN KEY (usuario_id) REFERENCES usuario(id),
  FOREIGN KEY (livro_id) REFERENCES livro(id)
 );
-
 -- Inserindo dados em nivel_associacao
 INSERT INTO nivel_associacao (nome) VALUES
 ('Regular'),
@@ -84,6 +81,7 @@ INSERT INTO usuario (nome, identificacao, email, data_cadastro, nivel_id) VALUES
 ('Lucas Martins', 'USR004', 'lucas@email.com', '2023-04-20', 4),
 ('Fernanda Alves', 'USR005', 'fernanda@email.com', '2023-05-30', 5),
 ('Rafael Pinto', 'USR006', 'rafael@email.com', '2023-06-18', 6);
+
 -- Inserindo dados em autor
 INSERT INTO autor (nome, data_nascimento, biografia) VALUES
 ('José Saramago', '1922-11-16', 'Autor português vencedor do Nobel.'),
@@ -100,6 +98,7 @@ INSERT INTO categoria (nome) VALUES
 ('Mistério'),
 ('Ficção Científica'),
 ('Biografia');
+
 -- Inserindo dados em livro
 INSERT INTO livro (titulo, isbn, descricao) VALUES
 ('Ensaio sobre a Cegueira', '978-85-01-00000-1', 'Romance sobre uma epidemia de cegueira.'),
@@ -109,7 +108,6 @@ INSERT INTO livro (titulo, isbn, descricao) VALUES
 ('O Caso dos Dez Negrinhos', '978-85-01-00000-5', 'Mistério com assassinatos em série.'),
 ('Perto do Coração Selvagem', '978-85-01-00000-6', 'Romance introspectivo de estreia de 
 Clarice.');
-
 -- Relacionando livros com autores
 INSERT INTO livro_autor (livro_id, autor_id) VALUES
 (1, 1),
@@ -136,47 +134,76 @@ data_devolucao) VALUES
 (5, 5, '2025-03-10', '2025-03-25', NULL),
 (6, 6, '2025-03-12', '2025-03-27', NULL);
 
--- 1. liste os livros
-SELECT * FROM livro;
+-- 1. Liste os livros.
+select * from livro;
 
--- 2. Cadastre um novo livro chamado "Python“, do autor "Eric Matthes", publicado em 24/04/2023, do gênero “técnico”. 
-SELECT * FROM autor;
-SELECT * FROM livro;
-INSERT INTO livro (titulo, isbn, descricao) VALUES ('Python', '‎978-8575224083', 'Este livro é orientado ao iniciante em programação.');
-SELECT * FROM autor WHERE nome = "Eric Matthes";
-INSERT INTO autor (nome, data_nascimento, biografia) VALUES ('Eric Matthes', '1972-01-01', 'Eric Matthes ensinou matemática e ciências no Ensino Médio por 25 anos');
-SELECT * FROM livro_autor;
-INSERT INTO livro_autor (livro_id, autor_id) VALUES (7, 7);
+-- INSERT
+-- 1. Cadastre um novo livro chamado "Python“, do autor "Eric Matthes", publicado em 24/04/2023, do gênero “técnico”.
+INSERT INTO livro(titulo, isbn, descricao)
+VALUES
+("Python", "9783127323207", "python para todos");
+
+INSERT INTO autor(nome, data_nascimento, biografia)
+VALUES 
+("Eric Matthes", "1972-01-01", "escritor");
+
+INSERT INTO livro_autor(livro_id, autor_id)
+VALUES (7, 7);
+
 ALTER TABLE livro ADD data_publicacao DATE;
-UPDATE livro SET data_publicacao = "2023-04-24" WHERE id = 7;
-SELECT * FROM categoria;
-INSERT INTO categoria (nome) VALUES ('técnico');
-SELECT * FROM livro_categoria;
-CREATE TABLE livro_categoria (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    livro_id INT NOT NULL,
-    categoria_id INT NOT NULL,
-    FOREIGN KEY (livro_id) REFERENCES livro(id),
-    FOREIGN KEY (categoria_id) REFERENCES categoria(id)
-);
-INSERT INTO livro_categoria (livro_id, categoria_id) VALUES (7, 7);
+UPDATE livro SET data_publicacao = "2023-04-24" where id = 7;
 
--- 1. Atualize o email do usuário id = 1 para teste@email.com. 
+INSERT INTO categoria(nome) VALUES ("técnico");
+INSERT INTO livro_categoria(livro_id, categoria_id) VALUES (7, 7);
+
+-- UPDATE
+-- 1. Atualize o email do usuário id = 1 para teste@email.com.
+UPDATE usuario SET email = "teste@email.com" WHERE id = 1;
+
 -- 2. Corrija o título do livro "Python" para " Curso Intensivo de Python: uma Introdução 
 -- Prática e Baseada em Projetos à Programação ". 
+UPDATE livro SET titulo = "Curso Intensivo de Python: uma Introdução Prática e Baseada em Projetos à Programação" WHERE id = 7;
+
 -- 3. Marque todos os livros publicados antes de 2000 como status = inativo. Se não 
--- houver nenhum, adicione 1 livro publicado antes de 2000. (Seu banco está pronto para 
--- aceitar o campo status?) 
+-- houver nenhum, adicione 1 livro publicado antes de 2000. (Seu banco está pronto para aceitar o campo status?)
+ALTER TABLE livro ADD status enum("ativo", "inativo");
 
-SELECT * FROM usuario WHERE id = 1;
-UPDATE usuario SET email = "teste@email.com" WHERE id = 1;
-SELECT * FROM livro;
-UPDATE livro SET titulo = " Curso Intensivo de Python: uma Introdução Prática e Baseada em Projetos à Programação" WHERE id = 7;
-SELECT * FROM status;
-CREATE TABLE status(
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    estado VARCHAR(10) NOT NULL
-);
-ALTER TABLE livro DROP COLUMN status_id;
-ALTER TABLE livro ADD CONSTRAINT status_id FOREIGN KEY (status_id) REFERENCES status(id);
+INSERT INTO livro (titulo, isbn, descricao, data_publicacao)
+VALUES
+("Livro antes de 2000", "3450345933", "esse livro foi escrito antes de 2000", "1999-09-27");
 
+UPDATE livro SET status = "inativo" WHERE DATE(data_publicacao) < "2000-01-01"; -- desative o safe mode!!!
+
+-- DELETE
+-- 1. Delete o livro com id = 2.
+DELETE FROM emprestimo WHERE livro_id = 2;
+DELETE FROM livro_autor WHERE livro_id = 2;
+DELETE FROM livro_categoria WHERE livro_id = 2;
+DELETE FROM livro WHERE id = 2;
+
+-- 2. Remova o usuário cujo nome é "Teste Testador". Se não houver, cadastre.
+INSERT INTO usuario(nome, identificacao, email, data_cadastro, nivel_id)
+VALUES
+("Teste Testador", "USR007", "testetestador@email.com", "2025-05-23", 1);
+
+DELETE FROM usuario WHERE nome = "Teste Testador";
+
+-- 3. Apague todos os livros com status "danificado". 
+ALTER TABLE livro
+MODIFY COLUMN status ENUM("ativo","inativo","danificado");
+
+UPDATE livro SET status = "danificado" WHERE id = 7;
+DELETE FROM emprestimo WHERE livro_id = 7;
+DELETE FROM livro_autor WHERE livro_id = 7;
+DELETE FROM livro_categoria WHERE livro_id = 7;
+
+DELETE FROM livro WHERE status = "danificado";
+
+-- 4. Delete os empréstimos realizados no ano de 2020. (Se não houver, insira)
+select * from usuario;
+select * from emprestimo;
+INSERT INTO emprestimo(usuario_id, data_emprestimo, data_limite, data_devolucao)
+VALUES
+(1, '2020-09-10', '2020-10-10', '2021-01-01');
+
+delete from emprestimo where year(data_emprestimo) = '2020';
